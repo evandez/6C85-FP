43e4808fb1fbb1936c1cc4dda0dde6c5f704b32c
Evan Hernandez <evanfredhernandez@gmail.com>
2024-05-12 21:13:44 -0400

    more pages be reformatted.

--- investor-profiles/src/pages/LocPage.tsx
+++ investor-profiles/src/pages/LocPage.tsx
@@ -1,68 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Button, Typography, Divider } from "@mui/material";
-import Plot from 'react-plotly.js';
+import React, { useEffect, useState } from "react";
+import { Typography, Divider } from "@mui/material";
+import Plot from "react-plotly.js";
 import { useLocation, useNavigate } from "react-router-dom";
+import TitleText from "../components/TitleText";
+import BodyText from "../components/BodyText";
+import VizContainer from "../components/VizContainer";
+import NavigationButtons from "../components/NavigationButtons";
+import Page from "../components/Page";
 
 const NEXT_PAGE = "/loc/viz";
 
 const LocPage = () => {
   const navigate = useNavigate();
-  const [mapc_map, set_mapc_map] = useState({data: [], layout: {}});
+  const [mapc_map, set_mapc_map] = useState({ data: [], layout: {} });
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   useEffect(() => {
-    fetch('/6C85-FP/loc_pre/mapc_map.json')  // Ensure this path is correct
-      .then(response => response.json())
-      .then(data => set_mapc_map(data))
-      .catch(error => console.error('Error loading GeoJSON data:', error));
+    fetch("/6C85-FP/loc_pre/mapc_map.json") // Ensure this path is correct
+      .then((response) => response.json())
+      .then((data) => set_mapc_map(data))
+      .catch((error) => console.error("Error loading GeoJSON data:", error));
   }, []);
 
   const handlePolygonClick = (event) => {
-    const loc = event.points[0].properties.subregion; 
-    navigate(`${NEXT_PAGE}/?${searchParams.toString()}&loc=${loc}`)
+    const loc = event.points[0].properties.subregion;
+    navigate(`${NEXT_PAGE}/?${searchParams.toString()}&loc=${loc}`);
   };
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 5,
-        textAlign: "left",
-        justifyContent: "center"
-      }}
-    >      <Typography variant="h4" gutterBottom>Choose an MAPC subregion</Typography>
-      <Typography variant="body1">
-        Click on a region to choose it.
-      </Typography>
+    <Page>
+      <TitleText>Choose an MAPC subregion</TitleText>
+      <BodyText>Click on a region to choose it.</BodyText>
       <Divider sx={{ mb: 2 }} />
 
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          gap: 2,
-          textAlign: "left",
-          justifyContent: "center",
-        }}
-      >
+      <VizContainer>
         {mapc_map.data.length ? (
           <Plot
             data={mapc_map.data}
             layout={mapc_map.layout}
-            style={{ width: "100%"}}
-            className="cursor-pointer"  // Add class name for cursor style
+            style={{ width: "100%", maxWidth: "800px" }}
+            className="cursor-pointer" // Add class name for cursor style
             onClick={handlePolygonClick}
           />
         ) : (
           <Typography>Loading first visualization...</Typography>
         )}
-      </Box>
+      </VizContainer>
 
-      <Button variant="contained" onClick={() => navigate(-1)} sx={{ mt: 2 }}>
-        Go Back
-      </Button>
-    </Box>
+      <NavigationButtons />
+    </Page>
   );
 };
 
--- investor-profiles/src/pages/LocVizPage.tsx
+++ investor-profiles/src/pages/LocVizPage.tsx
@@ -1,7 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { Box, Button, Typography, Divider } from "@mui/material";
 import { useLocation, useNavigate } from "react-router-dom";
-import Plot from 'react-plotly.js';
+import Plot from "react-plotly.js";
+import Page from "../components/Page";
+import NavigationButtons from "../components/NavigationButtons";
+import VizContainer from "../components/VizContainer";
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
 
 const NEXT_PAGE = "/flip";
 
@@ -10,28 +15,27 @@ function determineArticle(investorType: string): string {
   const type = investorType.toLowerCase();
 
   // Check if the investorType is equal to 'small', 'medium', or 'large'
-  if (type === 'small' || type === 'medium' || type === 'large') {
-    return 'a';
+  if (type === "small" || type === "medium" || type === "large") {
+    return "a";
   } else {
-    return 'an';
+    return "an";
   }
 }
 
 function getColorForInvestorType(investorType: string): string {
   switch (investorType.toLowerCase()) {
-    case 'institutional':
-      return 'red';
-    case 'small':
-      return 'blue';
-    case 'large':
-      return 'green';
-    case 'medium':
-      return 'orange';
+    case "institutional":
+      return "red";
+    case "small":
+      return "blue";
+    case "large":
+      return "green";
+    case "medium":
+      return "orange";
     default:
-      return ''; // Default to empty string or any other default color
+      return ""; // Default to empty string or any other default color
   }
 }
-
 
 const LocVizPage = () => {
   const navigate = useNavigate();
@@ -43,130 +47,88 @@ const LocVizPage = () => {
   const loc = searchParams.get("loc") ?? "ICC";
   const article = determineArticle(investorType);
   const inv_color = getColorForInvestorType(investorType);
-  const inv_text = (<span>{article} <span style={{ color: inv_color }}>{investorType}</span></span>);
-  const inv_text_no_article = (<span style={{ color: inv_color }}>{investorType}</span>);
+  const inv_text = (
+    <span>
+      {article} <span style={{ color: inv_color }}>{investorType}</span>
+    </span>
+  );
+  const inv_text_no_article = (
+    <span style={{ color: inv_color }}>{investorType}</span>
+  );
   // State to hold the data for the second Plotly plot based on the budget
-  const [plot_dash, set_plot_dash] = useState({data: [], layout: {}});
-  const [plot_index, set_plot_index] = useState({data: [], layout: {}});
+  const [plot_dash, set_plot_dash] = useState({ data: [], layout: {} });
+  const [plot_index, set_plot_index] = useState({ data: [], layout: {} });
 
   useEffect(() => {
     // Fetch for the second visualization based on investor type and budget
     fetch(`/6C85-FP/loc_post/${loc}_dashboard.json`)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         set_plot_dash(data);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
 
-      fetch(`/6C85-FP/loc_post/${investorType}_${budget}_${Style}_${loc}_index.json`)
-      .then(response => response.json())
-      .then(data => {
+    fetch(
+      `/6C85-FP/loc_post/${investorType}_${budget}_${Style}_${loc}_index.json`
+    )
+      .then((response) => response.json())
+      .then((data) => {
         set_plot_index(data);
       })
-      .catch(error => console.log(error));
-
+      .catch((error) => console.log(error));
   }, [investorType, budget, Style, loc]);
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 5,
-        textAlign: "left",
-        justifyContent: "center"
-      }}
-    >
-      {/* Introduction Text */}
-      <Typography variant="h5">
-        You have chosen to buy a property in the <b>{loc}</b> MAPC subregion
-      </Typography>
-      
-      <Typography variant="p" sx={{textAlign: "left"}}>
-        It is important to understand the demographics of the location you
-        are investing in as your investment may have a direct impact on their
-        housing prices. Here is a dashboad visualization of the demographics
-        of the {loc} MAPC subregion.
-      </Typography>
+    <Page>
+      <TitleText>
+        You chose to invest in the <b>{loc}</b> MAPC subregion.
+      </TitleText>
 
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          gap: 2,
-          textAlign: "left",
-          justifyContent: "center",
-        }}
-      >
+      <BodyText>
+        It is important to understand the demographics of the location you are
+        investing in as your investment may have a direct impact on their
+        housing prices. Here is a dashboad visualization of the demographics of
+        the {loc} MAPC subregion.
+      </BodyText>
+
+      <VizContainer>
         {plot_dash.data.length ? (
           <Plot
             data={plot_dash.data}
             layout={plot_dash.layout}
-            style={{ width: "100%"}}
+            style={{ width: "100%", maxWidth: "1200px" }}
           />
         ) : (
           <Typography>Loading first visualization...</Typography>
         )}
-      </Box>
+      </VizContainer>
 
-      <Divider/>
+      <Divider />
 
-      <Typography variant="p" sx={{textAlign: "left"}}>
-        We come back to the indices for making our choice.
-        The indices plot below shows the impact on choosing the {loc} subregions for 
-        {Style }style of properties with <b>${budget.toLocaleString()}</b> as {inv_text} investor.
-      </Typography>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          gap: 2,
-          textAlign: "left",
-          justifyContent: "center",
-        }}
-      >
+      <BodyText>
+        We come back to the indices for making our choice. The indices plot
+        below shows the impact on choosing the {loc} subregions for
+        {Style}style of properties with <b>${budget.toLocaleString()}</b> as{" "}
+        {inv_text} investor.
+      </BodyText>
+
+      <VizContainer>
         {plot_index.data.length ? (
           <Plot
             data={plot_index.data}
             layout={plot_index.layout}
-            style={{ width: "100%"}}
+            style={{ width: "100%", maxWidth: "800px" }}
           />
         ) : (
           <Typography>Loading first visualization...</Typography>
         )}
-      </Box>
+      </VizContainer>
 
-
-
-      {/* Navigation Buttons */}
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          gap: 2,
-        }}
-      >
-        <Button
-          variant="contained"
-          onClick={() => navigate(-1)}
-          sx={{
-            mt: 2,
-          }}
-        >
-          Go Back
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => navigate(`${NEXT_PAGE}/?${searchParams.toString()}`)}
-          sx={{
-            mt: 2,
-          }}
-        >
-          Next: Decide if you intend to flip your property
-        </Button>
-      </Box>
-    </Box>
+      <NavigationButtons
+        nextHref={`${NEXT_PAGE}/?${searchParams.toString()}`}
+        nextText="Decide to Flip"
+      />
+    </Page>
   );
 };
 
